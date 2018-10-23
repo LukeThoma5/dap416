92b08b2b591bd461fa4cdb7ada40bc2559bd74b9
LukeThoma5 <luke.4man@btinternet.com>
2018-10-23 11:57:39 +0100

    Make content more varied

--- home.php
+++ home.php
@@ -20,19 +20,29 @@ render_header("Home Page");
         }
     }
 
+    
+
 </script>
 
 <?php
 render_nav();
 
-function render_event_card($index) {
+$races = array(
+    array("title" => "2018 UCL Road World Championships", "image" => "images/winners.jpg"),
+    array("title" => "Stage 20 Post Race Coverage", "image" => "images/320-7R9A6117.jpg")
+);
+
+$render_event_card = function($index) use ($races) {
+    
+    $data = $races[$index % count($races)];
+    
     echo <<<eventCard
     \n
     <div class="sizable-card">
     <div class="card bg-dark img-card" tabindex="$index" onclick="onCardClick(event)" onKeyPress="onCardClick(event)" role="button">
-        <div class="hide-overspill"><img class="card-img" src="images/winners.jpg" alt="Card image"></div>
+        <div class="hide-overspill"><img class="card-img" src="$data[image]" alt="Card image"></div>
         <div class="card-img-overlay">
-            <h5 class="card-title">Card title</h5>
+            <h5 class="card-title">$data[title]</h5>
         </div>
     </div>    
     </div>
@@ -48,7 +58,7 @@ eventCard;
                 
                 <?php
                     for($i = 0; $i < 10; $i++) {
-                        render_event_card($i);
+                        $render_event_card($i);
                     }
                 ?>
             </section>
