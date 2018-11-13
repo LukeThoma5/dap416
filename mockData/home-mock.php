7f58ca5019a093de3e85ad7459615269854656df
LUKE THOMAS GEORGE FOREMAN <LFOREMA1@stu.chi.ac.uk>
2018-11-13 11:31:36 +0000

    Auto prefix and image optimisations

--- mockData/home-mock.php
+++ mockData/home-mock.php
@@ -1,8 +1,8 @@
 <?php
 
 $races = array(
-    array("title" => "2018 UCL Road World Championships", "image" => "images/winners.jpg", "desc" => "Image of the winners of the 2018 UCL Road World Championships"),
-    array("title" => "Stage 20 Post Race Coverage", "image" => "images/320-7R9A6117.jpg", "desc" => "Image of the winners of Stage 20 of Post Race Coverage")
+    array("title" => "2018 UCL Road World Championships", "image" => "winners", "desc" => "Image of the winners of the 2018 UCL Road World Championships"),
+    array("title" => "Stage 20 Post Race Coverage", "image" => "winners2", "desc" => "Image of the winners of Stage 20 of Post Race Coverage")
 );
 
 
